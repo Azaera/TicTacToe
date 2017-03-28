af272ca99842ef496f866670031c61574bfd47ca
Azaera <bjorn.alfsson@gmail.com>
2017-03-28 21:52:10 +0200

    Added player names

--- app/src/main/java/com/example/bjorn/tictactoe/Board.java
+++ app/src/main/java/com/example/bjorn/tictactoe/Board.java
@@ -1,9 +1,14 @@
 package com.example.bjorn.tictactoe;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Board extends AppCompatActivity implements View.OnClickListener{
@@ -16,11 +21,50 @@ public class Board extends AppCompatActivity implements View.OnClickListener{
     private Button seven;
     private Button eight;
     private Button nine;
+    private TextView playerOne;
+    private TextView playerTwo;
+    final Context context = this;
+    private int player = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        playerOne = ( TextView ) findViewById(R.id.firstPlayer);
+        playerTwo = ( TextView ) findViewById(R.id.secondPlayer);
+
+        //First player
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("PLAYERS");
+        builder.setMessage("Enter first players name");
+        final EditText input2 = new EditText(this);
+        builder.setView(input2);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name1 = input2.getText().toString();
+                playerOne.setText(name1);
+            }
+        });
+        builder.create().show();
+
+        //Second player
+        AlertDialog.Builder builders = new AlertDialog.Builder(this);
+        builders.setTitle("PLAYERS");
+        builders.setMessage("Enter second players name");
+        final EditText input = new EditText(this);
+        builders.setView(input);
+
+        builders.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name2 = input.getText().toString();
+                playerTwo.setText(name2);
+            }
+        });
+        builders.create().show();
 
         one = ( Button ) findViewById(R.id.one);
         two = ( Button ) findViewById(R.id.two);
