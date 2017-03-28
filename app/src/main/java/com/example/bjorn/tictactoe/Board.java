fbf0545a363ea1da92a8e97d32ea952d8b8dca45
Azaera <bjorn.alfsson@gmail.com>
2017-03-28 14:24:34 +0200

    Twoplayer part started

--- app/src/main/java/com/example/bjorn/tictactoe/Board.java
+++ app/src/main/java/com/example/bjorn/tictactoe/Board.java
@@ -2,12 +2,82 @@ package com.example.bjorn.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Board extends AppCompatActivity {
+public class Board extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        Button one = ( Button ) findViewById(R.id.one);
+        Button two = ( Button ) findViewById(R.id.two);
+        Button three = ( Button ) findViewById(R.id.three);
+        Button four = ( Button ) findViewById(R.id.four);
+        Button five = ( Button ) findViewById(R.id.five);
+        Button six = ( Button ) findViewById(R.id.six);
+        Button seven = ( Button ) findViewById(R.id.seven);
+        Button eight = ( Button ) findViewById(R.id.eight);
+        Button nine = ( Button ) findViewById(R.id.nine);
+
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+        five.setOnClickListener(this);
+        six.setOnClickListener(this);
+        seven.setOnClickListener(this);
+        eight.setOnClickListener(this);
+        nine.setOnClickListener(this);
+    }
+
+    public Board() {
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+
+            case R.id.one:
+                Toast.makeText(this, "Button 1", Toast.LENGTH_LONG).show();
+
+                break;
+
+            case R.id.two:
+                Toast.makeText(this, "Button 2", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.three:
+                Toast.makeText(this, "Button 3", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.four:
+                Toast.makeText(this, "Button 4", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.five:
+                Toast.makeText(this, "Button 5", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.six:
+                Toast.makeText(this, "Button 6", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.seven:
+                Toast.makeText(this, "Button 7", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.eight:
+                Toast.makeText(this, "Button 8", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nine:
+                Toast.makeText(this, "Button 9", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/bjorn/tictactoe/StartGame.java
+++ app/src/main/java/com/example/bjorn/tictactoe/StartGame.java
@@ -1,7 +1,10 @@
 package com.example.bjorn.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartGame extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class StartGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+
+        Button twoPlayers = ( Button )findViewById(R.id.twoPlayer);
+
+        twoPlayers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartGame.this, Board.class);
+                intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
